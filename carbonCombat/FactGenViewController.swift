f305542338ba2daaf86eae4e5b07768c85942536
Elyse Ellingsworth <wildelephant05@icloud.com>
2021-06-16 20:52:19 -0600

    LOTS of design changes, changed facts as well.

--- carbonCombat/FactGenViewController.swift
+++ carbonCombat/FactGenViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class FactGenViewController: UIViewController {
     @IBOutlet weak var factGenTextBox: UILabel!
-    let facts = ["Carbon footprint: the amount of carbon dioxide and other carbon compounds emitted due to the consumption of fossil fuels by a person, group, etc.", "The average US household footprint is 20 tons, while the world average is only 4 tons.","The US produces 14% of global emissions, second only to China at 22%.", "Meat products have a higher footprint than grain/vegetable products because of the methane released from animals.", "Ruminants (cattle, sheep, goats) produced 178 million metric tons of enteric methane in the US in 2018.", "Shifting to a vegetarian meal one meal a day can save the equivalent greenhouse gases of driving 1,160 miles.", "Eating a vegetarian or pescetarian diet is better for the environment than a diet which includes a lot of meat.", "Your water has a high carbon footprint.", "Food accounts for 10-30% of a household’s carbon footprint.", "Energy consumed by devices in standby mode accounts for 5-10% of residential energy use, adding up to $100 per year for the average American household.", "Usually, the bulk of an individual’s carbon footprint will come from transportation, housing and food.", "Learning the 5 R's - refuse, reduce, reuse, rot, recycle - can greatly help to reduce your carbon footprint!"]
+    
+    @IBOutlet weak var doFactGenButtonRounded: UIButton!
+    let facts = ["first things first: your carbon footprint is a measure of the impact your activities have on the amount of carbon dioxide (CO2) produced through the burning of fossil fuels", "the average US household footprint is 20 tons, or 32,000 pounds.","the US produces 14% of global emissions, second only to China at 22%.", "meat products have a higher footprint than grain/vegetable products because of the methane released from animals.", "ruminants (cattle, sheep, goats) produced 178 million metric tons of methane in the US in 2018.", "shifting to a vegetarian meal one meal a day can save the equivalent greenhouse gases of driving 1,160 miles.", "eating a vegetarian or pescetarian diet is better for the environment than a meat-heavy diet", "the water you drink has a high carbon footprint.", "food accounts for 10-30% of a household’s carbon footprint.", "energy consumed by devices in standby mode accounts for 5-10% of residential energy use, adding up to $100 per year for the average American household.", "usually, the bulk of your carbon footprint will come from transportation, housing and food.", "learning the 5 R's - refuse, reduce, reuse, rot, recycle - can greatly help to reduce your carbon footprint!"]
     var randomIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         factGenTextBox.text = facts[randomIndex]
+        doFactGenButtonRounded.layer.cornerRadius = 20
+        doFactGenButtonRounded.layer.borderWidth = 10
+        doFactGenButtonRounded.layer.borderColor = CGColor(red: 233 / 255, green: 243 / 255, blue: 235 / 255, alpha: 1)
         // Do any additional setup after loading the view.
     }
     
