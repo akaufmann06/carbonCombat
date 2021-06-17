3cba5c2abf38acc1260d13660e322a1d1c5da67e
Elyse Ellingsworth <wildelephant05@icloud.com>
2021-06-17 10:50:07 -0600

    made quiz view controller and file to connect it.

---
+++ carbonCombat/QuizViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuizViewController.swift
+//  carbonCombat
+//
+//  Created by Elyse Ellingsworth on 6/17/21.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
